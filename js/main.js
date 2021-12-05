0f93d0bf1a51e4e0a8bd71a291afb24ef6129efc
Jesús Valdez <jsvaldezv@yahoo.com>
2021-12-04 23:31:34 -0600

    Send mail

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ const navShow = document.querySelector('.header-links-peque');
 const nav = document.querySelector('.header-peque');
 const idioma = document.querySelector('.idioma');
 const idiomaPhone = document.querySelector('.idiomaPhone');
+const form = document.querySelector(".contact_form");
+const sendEsp = document.querySelector(".sendEsp")
+const sendEn = document.querySelector(".sendEn")
 const tMenu = new TimelineMax();
 //const arrow = document.querySelector('#arrow');
 
@@ -91,7 +94,36 @@ function changeIdioma()
   }
 }
 
+  //CORREO
+function sendMail(e)
+{
+  e.preventDefault();
+
+  const name = document.querySelector(".name");
+  const subject = document.querySelector(".subject");
+  const mail = document.querySelector(".mail");
+  const text = document.querySelector(".text");
+
+  console.log(mail.value)
+
+  Email.send({
+    SecureToken : "bf4105b9-a183-47ad-b177-5c5e0597aaf9",
+    To : 'jsvaldezv@gmail.com',
+    From : mail.value,
+    Subject : subject.value,
+    Body : text.value
+  }).then(
+    message => alert(message)
+  );
+}
+
 // LISTENERS
+idioma.addEventListener('click', changeIdioma);
+idiomaPhone.addEventListener('click', changeIdioma);
+form.addEventListener('submit', sendMail);
+//sendEsp.addEventListener('submit', sendMail);
 //arrow.addEventListener('click', changeSkills)
-idioma.addEventListener('click', changeIdioma)
-idiomaPhone.addEventListener('click', changeIdioma)
+
+//7BC6567B20F32C4B02290CC874905D17FC3A
+//smtp.elasticemail.com
+//2525
