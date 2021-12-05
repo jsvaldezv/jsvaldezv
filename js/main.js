1eaf359125614d1ec44df9b3cc6089e6c7a2111a
Jesús Valdez <jsvaldezv@yahoo.com>
2021-12-05 00:02:48 -0600

    Try with PHP

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-import {Email} from "https://smtpjs.com/v3/smtp.js";
-
 // DECLARACIONES
 const navOpen = document.querySelector('.header-peque');
 const navClose = document.querySelector('#close');
@@ -7,14 +5,10 @@ const navShow = document.querySelector('.header-links-peque');
 const nav = document.querySelector('.header-peque');
 const idioma = document.querySelector('.idioma');
 const idiomaPhone = document.querySelector('.idiomaPhone');
-const form = document.querySelector(".contact_form");
-const sendEsp = document.querySelector(".sendEsp")
-const sendEn = document.querySelector(".sendEn")
-const tMenu = new TimelineMax();
 //const arrow = document.querySelector('#arrow');
 
-let show = false;
 let idiom = "espanol"
+//let show = false;
 
 // MENU RESPONSIVE
 if(navOpen)
@@ -40,34 +34,6 @@ if(navClose){
         nav.style.display = 'block';
     })
 }
-
-// CHANGE SKILLS IN HTML
-/*function changeSkills()
-{
-  const techs = document.querySelectorAll(".showTech");
-  const other = document.querySelectorAll(".showOther");
-
-  if(!show)
-  {
-    techs[0].style.display = 'none';
-    techs[1].style.display = 'none';
-    techs[2].style.display = 'none';
-    other[0].style.display = 'block';
-    other[1].style.display = 'block';
-    other[2].style.display = 'block';
-    show = true;
-  }
-  else
-  {
-    techs[0].style.display = 'block';
-    techs[1].style.display = 'block';
-    techs[2].style.display = 'block';
-    other[0].style.display = 'none';
-    other[1].style.display = 'none';
-    other[2].style.display = 'none';
-    show = false;
-  }
-}*/
 
 // CHANGE IDIOMA
 function changeIdioma()
@@ -96,36 +62,34 @@ function changeIdioma()
   }
 }
 
-  //CORREO
-function sendMail(e)
-{
-  e.preventDefault();
-
-  const name = document.querySelector(".name");
-  const subject = document.querySelector(".subject");
-  const mail = document.querySelector(".mail");
-  const text = document.querySelector(".text");
-
-  console.log(mail.value)
-
-  Email.send({
-    SecureToken : "bf4105b9-a183-47ad-b177-5c5e0597aaf9",
-    To : 'jsvaldezv@gmail.com',
-    From : mail.value,
-    Subject : subject.value,
-    Body : text.value
-  }).then(
-    message => alert(message)
-  );
-}
-
 // LISTENERS
 idioma.addEventListener('click', changeIdioma);
 idiomaPhone.addEventListener('click', changeIdioma);
-form.addEventListener('submit', sendMail);
-//sendEsp.addEventListener('submit', sendMail);
-//arrow.addEventListener('click', changeSkills)
 
-//7BC6567B20F32C4B02290CC874905D17FC3A
-//smtp.elasticemail.com
-//2525
+// CHANGE SKILLS IN HTML
+/*function changeSkills()
+{
+  const techs = document.querySelectorAll(".showTech");
+  const other = document.querySelectorAll(".showOther");
+
+  if(!show)
+  {
+    techs[0].style.display = 'none';
+    techs[1].style.display = 'none';
+    techs[2].style.display = 'none';
+    other[0].style.display = 'block';
+    other[1].style.display = 'block';
+    other[2].style.display = 'block';
+    show = true;
+  }
+  else
+  {
+    techs[0].style.display = 'block';
+    techs[1].style.display = 'block';
+    techs[2].style.display = 'block';
+    other[0].style.display = 'none';
+    other[1].style.display = 'none';
+    other[2].style.display = 'none';
+    show = false;
+  }
+}*/
---
+++ php/enviar.php
@@ -0,0 +1,28 @@
+<?php
+if(isset($_POST['acepto'])){
+    if(!empty($_POST['name']) && !empty($_POST['mail']) && !empty($_POST['message']))
+    {
+        $name = $_POST['name'];
+        $correo = $_POST['mail'];
+
+        $header = 'From: ' . "earcandytech.com" . "\r\n";
+        $header .= "X-Mailer: PHP/" . phpversion() . "\r\n";
+        $header .= "Mime-Version: 1.0 \r\n";
+        $header .= "Content-Type: text/plain";
+
+        $message = "Nombre: " . $name . "\r\n";
+        $message .= "Correo: " . $correo . "\r\n";
+        $message .= "Mensaje: " . $_POST['message'] . "\r\n";
+        $message .= "Enviado el: " . date('d/m/Y', time());
+
+        $para = 'jsvaldezv@gmail.com';
+        $asunto = 'Contacto Portafolio';
+
+        $mail = mail($para, utf8_decode($asunto), utf8_decode($message), $header);
+
+        header("Location:../index.html#form");
+
+        echo("Enviado");
+    }
+}
+?>
